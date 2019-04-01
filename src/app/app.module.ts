9a5353187bef64ebe949ca6352f790bd5d2a8883
Dhia Rzig <dhia.rzig@outlook.com>
2019-04-01 18:40:49 +0100

    SPA p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,27 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -28,9 +41,10 @@ import { DishService } from './services/dish.service';
     MatCardModule,
     MatButtonModule,
     MatListModule,
+    AppRoutingModule,
     FlexLayoutModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
