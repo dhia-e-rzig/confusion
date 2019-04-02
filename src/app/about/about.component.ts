a5cd0220db5f01e6b3d5d7a5d35db2eb8a9d2426
Dhia Rzig <dhia.rzig@outlook.com>
2019-04-02 20:20:09 +0100

    angular part 2 complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader';
+import {LeaderService} from '../services/leader.service'
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  leaders:Leader[]
+  constructor(private LeaderService: LeaderService) { }
 
   ngOnInit() {
+    this.leaders=this.LeaderService.getLeaders();
   }
 
 }
